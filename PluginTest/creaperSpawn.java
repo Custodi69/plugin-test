e2d56c44ebf5822e844d3ee1470065ab370cbcc5
Custodi69 <77865559+Custodi69@users.noreply.github.com>
2021-01-22 22:27:39 -0500

    Add files via upload

---
+++ PluginTest/creaperSpawn.java
@@ -0,0 +1,30 @@
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+
+
+public class creaperSpawn implements CommandExecutor{
+
+    public creaperSpawn(Main plugin) {
+        plugin.getCommand("creepspawn").setExecutor(this);
+    }
+    @Override
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player p = (Player) sender;
+        Location loc = p.getLocation();
+        World w = p.getWorld();
+
+        for (int i = 0; i < 10; i++) {
+            w.spawnEntity(loc, EntityType.CREEPER);
+        }
+
+        return false;
+    }
+    
+}
